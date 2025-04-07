08e5628f529190aea40c15bca588223428946e01
mariahj990 <131026919+mariahj990@users.noreply.github.com>
2025-04-07 17:53:24 -0400

    Connected to Mongo

--- debug_ducks/src/app/page.tsx
+++ debug_ducks/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Content from '../components/Content';
 import Welcome from '../components/Welcome';
+import connectMongoDB from "../../config/mongodb";
 
 export default function Home() {
+  //connectMongoDB();
   return (
     <Content>
       <Welcome />
